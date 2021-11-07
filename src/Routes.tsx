2710af6468e8bf7564919c44f08d92a53bee0d82
sunny_thinkpad <coolcanman987@gmail.com>
2021-11-07 21:53:49 +1100

    feat: init commit

---
+++ src/Routes.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+const Routes = () => {
+    <Switch>
+        <Route exact path="/home">
+            {/* some page here */}
+        </Route>
+    </Switch>
+}
