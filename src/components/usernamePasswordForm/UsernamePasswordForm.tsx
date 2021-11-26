d876458b168e53ef36bc402f7b0e033be8f19787
sunny_thinkpad <sunnyw987@gmail.com>
2021-11-26 23:21:49 +1100

    feat: implement username and password component skeleton

---
+++ src/components/usernamePasswordForm/UsernamePasswordForm.tsx
@@ -0,0 +1,38 @@
+import React, { FC } from "react";
+
+type FormProps = {
+  // TODO: have additional css props for input?
+  usernameValue: string;
+  passwordValue: string;
+  handleUsernameChange: (username: string) => void;
+  handlePasswordChange: (password: string) => void;
+}
+
+const UsernamePasswordForm: FC<FormProps> = ({
+  usernameValue,
+  passwordValue,
+  handleUsernameChange,
+  handlePasswordChange,
+}) => {
+
+  return (
+    <form>
+        <input
+          type="text"
+          placeholder="username"
+          value={usernameValue}
+          onChange={(e) => handleUsernameChange(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="password"
+          value={passwordValue}
+          onChange={(e) => handlePasswordChange(e.target.value)}
+        />
+        <input type="submit"/>
+    </form>
+  );
+};
+
+
+export default UsernamePasswordForm;
--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { Button } from "../../components/button/Button";
+import UsernamePasswordForm from "../../components/usernamePasswordForm/UsernamePasswordForm";
 import { palette } from "../../components/Palette";
 import { Banner, InputSection, LoginContainer } from "./style";
 
 const LandingPage = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const updateUsername = useCallback((username) => {
+    setUsername(username);
+  }, [setUsername]);
+
+  const updatePassword = useCallback((password) => {
+    setPassword(password);
+  }, [setPassword]);
+
+  // TODO: will need to handle onsubmit 
+  // const handleSubmit = (e: Event) => {
+  //   e.preventDefault();
+  //   // console.log(`the username is ${username} and the password is ${password}`);
+  // }
+
   return (
     <LoginContainer>
       <Banner>
         Login
       </Banner>
       <InputSection>
-      {/* TODO: extract input to be another component with placeholder, length, height, font-size props? */}
-        <input placeholder="Username"></input>
-        <input placeholder="Password"></input>
+        <UsernamePasswordForm
+          usernameValue={username}
+          passwordValue={password}
+          handleUsernameChange={updateUsername}
+          handlePasswordChange={updatePassword}
+        />
       </InputSection>
       <Button
         backgroundColor={palette.lightTeal}
