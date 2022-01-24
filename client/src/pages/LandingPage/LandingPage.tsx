6216d05f65063f232aa85a8097ed9a2d747b0e43
sunny_thinkpad <sunnyw987@gmail.com>
2022-01-24 22:35:09 +1100

    feat: add basic menu to landing page

--- client/src/pages/LandingPage/LandingPage.tsx
+++ client/src/pages/LandingPage/LandingPage.tsx
@@ -1,25 +1,47 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
-import { Button } from "../../components/button/Button";
 import { Navbar, LandingPageContainer } from "./style";
+import { AccountCircle, Logout } from "@mui/icons-material";
+import { Menu, IconButton, MenuItem } from "@mui/material";
 
 const LandingPage = () => {
   const { location } = useHistory();
   const user = location.state;
 
+  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+  const [menuOpen, setMenuOpen] = useState<boolean>(false);
+
   useEffect(() => {
     console.log(user);
   });
 
+  const handleMenuOpen = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(e.currentTarget);
+    setMenuOpen(true);
+  }
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    setMenuOpen(false);
+  }
+
   return (
     <LandingPageContainer>
       <Navbar>
-        <Button
-          width="auto"
-          height="auto"
+        <IconButton
+          onClick={handleMenuOpen}
         >
-          .dropDownAvatar()
-        </Button>
+          <AccountCircle 
+            sx={{ fontSize: 50, color: "white"}}
+          />
+        </IconButton>
+        <Menu
+          open={menuOpen}
+          onClose={handleMenuClose}
+        >
+          <MenuItem>Logout</MenuItem>
+        </Menu>
+        
       </Navbar>
     </LandingPageContainer>
   );
